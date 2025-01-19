8922b3cf4c3648587c718717f7cea63c9104b091
Santo Rasu <santorasu09.com>
2025-01-20 01:32:49 +0600

    Responsive portfolio

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,12 @@
 import './About.css';
-import theme_pattern from '../../assets/theme_pattern.svg';
+// import theme_pattern from '../../assets/theme_pattern.svg';
 import profile_img from '../../assets/rasu.png';
 const About = () => {
     return (
         <div className='about'>
             <div className="about-title">
                 <h1>About Me</h1>
-                <img src={theme_pattern} alt="" />
+                {/* <img src={theme_pattern} alt="" /> */}
             </div>
 
             <div className="about-section">
